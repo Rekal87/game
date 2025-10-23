ef4d14291c9f6db7d5a46424c4739c0676712d6b
Nnamdi Echem <nnamdi.b@gmail.com>
2025-10-23 20:12:23 +0200

    added conditional rendering

--- src/components/ListGroup.jsx
+++ src/components/ListGroup.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
 function ListGroup() {
-  return <h1>List Group</h1>;
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>List</h1>
+
+      {items.length === 0 && <p>No items found</p>}
+
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            key={item}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
